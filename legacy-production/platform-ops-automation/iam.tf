81631efe091ee5a5f7947bb3b441cfa6275f2c3d
Cuong (Bill) M. NGO <cuong.ngominh@plenti.com.au>
2025-09-18 16:07:06 +0700

    Allow start instance refresh

--- legacy-production/platform-ops-automation/iam.tf
+++ legacy-production/platform-ops-automation/iam.tf
@@ -1,6 +1,6 @@
 module "pipeline_tls_automation_role" {
   source  = "app.terraform.io/plentiau/iam-role-wizard/aws"
-  version = "2.3.0"
+  version = "2.4.0"
 
   name               = "pipeline-tls-automation-role"
   oidc_provider_arn  = "arn:aws:iam::519527725796:oidc-provider/token.actions.githubusercontent.com"
@@ -10,7 +10,10 @@ module "pipeline_tls_automation_role" {
     "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/test/services/*",
     "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/production/services/*"
   ]
-
+  allow_start_instance_refresh_asg_arn = [
+    "arn:aws:autoscaling:ap-southeast-2:519527725796:autoScalingGroup:*:autoScalingGroupName/uat-linux-host-pool-*",
+    "arn:aws:autoscaling:ap-southeast-2:519527725796:autoScalingGroup:*:autoScalingGroupName/*Service*"
+  ]
 }
 
 output "pipeline_tls_automation_role_arn" {
