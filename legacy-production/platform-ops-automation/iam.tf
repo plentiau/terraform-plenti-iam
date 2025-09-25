686a24922840e305fe5e6338d03a6c347fc9ef33
Cuong (Bill) M. NGO <cuong.ngominh@plenti.com.au>
2025-09-25 15:45:16 +0700

    Increase maximum session duration

--- legacy-production/platform-ops-automation/iam.tf
+++ legacy-production/platform-ops-automation/iam.tf
@@ -2,10 +2,11 @@ module "pipeline_tls_automation_role" {
   source  = "app.terraform.io/plentiau/iam-role-wizard/aws"
   version = "2.6.1"
 
-  name               = "pipeline-tls-automation-role"
-  oidc_provider_arn  = "arn:aws:iam::519527725796:oidc-provider/token.actions.githubusercontent.com"
-  github_repo_names  = ["platform-ops-automation:*"]
-  allow_list_secrets = true
+  name                 = "pipeline-tls-automation-role"
+  max_session_duration = 21600 # 6 hours
+  oidc_provider_arn    = "arn:aws:iam::519527725796:oidc-provider/token.actions.githubusercontent.com"
+  github_repo_names    = ["platform-ops-automation:*"]
+  allow_list_secrets   = true
   allow_get_secret_value = [
     "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/test/services/*",
     "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/production/services/*"
