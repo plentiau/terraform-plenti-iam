fc2639387d0952e6ba3ddd0fb902eeecaf2e02fc
Cuong (Bill) M. NGO <cuong.ngominh@plenti.com.au>
2025-09-25 09:53:00 +0700

    Allow update secret values

--- legacy-production/platform-ops-automation/iam.tf
+++ legacy-production/platform-ops-automation/iam.tf
@@ -1,12 +1,16 @@
 module "pipeline_tls_automation_role" {
   source  = "app.terraform.io/plentiau/iam-role-wizard/aws"
-  version = "2.4.0"
+  version = "2.5.0"
 
   name               = "pipeline-tls-automation-role"
   oidc_provider_arn  = "arn:aws:iam::519527725796:oidc-provider/token.actions.githubusercontent.com"
   github_repo_names  = ["platform-ops-automation:*"]
   allow_list_secrets = true
   allow_get_secret_value = [
+    "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/test/services/*",
+    "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/production/services/*"
+  ]
+  allow_update_secret_value = [
     "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/test/services/*",
     "arn:aws:secretsmanager:ap-southeast-2:519527725796:secret:/production/services/*"
   ]
