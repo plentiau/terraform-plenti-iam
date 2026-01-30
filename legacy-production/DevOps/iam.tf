29f82c576fe4465ac040178f28cbedb1469bdf83
plenti-bill-ngo <94506177+plenti-bill-ngo@users.noreply.github.com>
2026-01-30 16:43:52 +0700

    feat: Setup IAM roles for regular monitor workflows (#13)

--- legacy-production/DevOps/iam.tf
+++ legacy-production/DevOps/iam.tf
@@ -14,3 +14,38 @@ module "pipeline_automate_microservice_consumer_role" {
 output "pipeline_automate_microservice_consumer_role_arn" {
   value = module.pipeline_automate_microservice_consumer_role.iam_role_arn
 }
+
+module "pipeline_regular_uat_monitor_role" {
+  source  = "app.terraform.io/plentiau/iam-role-wizard/aws"
+  version = "2.6.1"
+
+  name              = "pipeline-regular-uat-monitor-role"
+  oidc_provider_arn = "arn:aws:iam::519527725796:oidc-provider/token.actions.githubusercontent.com"
+  github_repo_names = ["DevOps:*"]
+  allow_get_ssm_parameter_arns = [
+    "arn:aws:ssm:ap-southeast-2:519527725796:parameter/password_dbscripts_uat",
+    "arn:aws:ssm:ap-southeast-2:519527725796:parameter/devops/*"
+  ]
+}
+
+output "pipeline_regular_uat_monitor_role_arn" {
+  value = module.pipeline_regular_uat_monitor_role.iam_role_arn
+}
+
+module "pipeline_regular_prod_monitor_role" {
+  source  = "app.terraform.io/plentiau/iam-role-wizard/aws"
+  version = "2.6.1"
+
+  name              = "pipeline-regular-prod-monitor-role"
+  oidc_provider_arn = "arn:aws:iam::519527725796:oidc-provider/token.actions.githubusercontent.com"
+  github_repo_names = ["DevOps:*"]
+  allow_get_ssm_parameter_arns = [
+    "arn:aws:ssm:ap-southeast-2:519527725796:parameter/password_production_monitor",
+    "arn:aws:ssm:ap-southeast-2:519527725796:parameter/aurora_password_dbreadonly",
+    "arn:aws:ssm:ap-southeast-2:519527725796:parameter/devops/*"
+  ]
+}
+
+output "pipeline_regular_prod_monitor_role_arn" {
+  value = module.pipeline_regular_prod_monitor_role.iam_role_arn
+}
